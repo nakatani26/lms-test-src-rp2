ab62467e3d1654feb84b002c16a238cbda6e5ecd
a-nakatani <a-nakatani@kf-c.co.jp>
2025-10-23 16:15:21 +0900

    fix：Case03.java　受講生ログイン　正常系

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト ログイン機能①
@@ -34,15 +41,44 @@ public class Case03 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		goTo("http://localhost:8080/lms");
+
+		assertEquals("ログイン", webDriver.findElement(By.tagName("h2")).getText());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+		
+		WebElement loginId = webDriver.findElement(By.name("loginId"));
+		WebElement loginPassword = webDriver.findElement(By.name("password"));
+
+		// ログインID及びパスワードのクリア処理
+		loginId.clear();
+		loginPassword.clear();
+
+		// ログインID及びパスワード入力処理
+		loginId.sendKeys("StudentAA01");
+		loginPassword.sendKeys("StudentAA02");
+		
+		//ログインボタン押下
+		WebElement loginButton = webDriver.findElement(By.className("btn"));
+		loginButton.click();
+		
+		new WebDriverWait(webDriver, Duration.ofSeconds(10))
+        .until(ExpectedConditions.urlContains("/course/detail"));
+		
+		assertEquals("http://localhost:8080/lms/course/detail", webDriver.getCurrentUrl());
+		
+		getEvidence(new Object() {
+		});
+	}
+		
 	}
 
-}
+
