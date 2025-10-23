6d92081487f1b3db39bb3f0f2fff69cb8e26b319
a-nakatani <a-nakatani@kf-c.co.jp>
2025-10-23 11:11:25 +0900

    fix：Case02.java 受講生ログイン認証失敗
    期待値
    「ログインに失敗しました。」というエラーメッセージが表示される。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト ログイン機能①
@@ -34,15 +41,57 @@ public class Case02 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+			// トップページ
+			goTo("http://localhost:8080/lms");
+
+			assertEquals("ログイン", webDriver.findElement(By.tagName("h2")).getText());
+
+			getEvidence(new Object() {
+			});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+		
+		WebElement loginId = webDriver.findElement(By.name("loginId"));
+		WebElement loginPassword = webDriver.findElement(By.name("password"));
+		
+		// ログインID及びパスワードのクリア処理
+		loginId.clear();
+		loginPassword.clear();
+		
+		// ログインID及びパスワード入力処理
+		loginId.sendKeys("StudentAA1000");
+		loginPassword.sendKeys("StudentAA1000");
+		
+		//ログインボタンを押下
+		WebElement loginButton = webDriver.findElement(By.className("btn"));
+		loginButton.click();
+		
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+		WebElement errorMsg = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".help-inline.error"))
+				);
+		
+		getEvidence(new Object() {});
+		String actualMessage = errorMsg.getText().trim();
+		assertEquals("ログインに失敗しました。", actualMessage);
+		
+	
+		
+//		//ログインに失敗
+//		WebElement errorMsg = webDriver.findElement(By.cssSelector(".help-inline.error"));
+//		String actualMessage = errorMsg.getText();
+//		assertEquals("ログインに失敗しました",actualMessage);
+//		Object evidenceInstance = new Object() {};
+////		getEvidence(new Object() {
+////		});
+//		getEvidence(this);
+
+		
 	}
 
 }
